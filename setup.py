ba4ce77f8fe2e0d88139a043db7741936381d081
Niko Fink <nfink95@gmail.com>
2017-02-17 16:18:20 +0100

    initial commit

---
+++ setup.py
@@ -0,0 +1,31 @@
+from codecs import open
+from os import path
+
+from setuptools import find_packages, setup
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='mensabot',
+    version='0.1.0',
+    description='telegram bot for uni passau mensa',
+    long_description=long_description,
+    url='https://github.com/N-Coder/mensabot',
+    author='Niko Fink',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.5',
+    ],
+    packages=find_packages(),
+    install_requires=[
+        'dateparser',
+        'requests',
+        'beautifulsoup4',
+        'humanize'
+    ]
+)
