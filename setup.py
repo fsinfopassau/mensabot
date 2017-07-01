70fb8e7ee499fcf36a92ac7212af0d15bd1bf78b
Niko Fink <nfink95@gmail.com>
2017-07-01 21:02:05 +0200

    released version 0.3.0
    - check for modified menu and notify users on change
    - edit sent menu messages when the stwno menu is updated
    - add further config options for change notifications
    - automatically increment date for push notifications after mensa closes
    - send all config options for empty get
    - switch today's menu at the time the mensa closes
    - reset mensa notifications when mensa closes
    - fixed notifications when push time is after mensa closes

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='mensabot',
-    version='0.2.6',
+    version='0.3.0',
     description='telegram bot for uni passau mensa',
     long_description=long_description,
     url='https://github.com/N-Coder/mensabot',
