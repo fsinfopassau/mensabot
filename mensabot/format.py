8e3b67f6955dde150b3626ca09f9c4c1502c6be3
Sven Gebauer <mail@sgebauer.net>
2024-09-03 22:18:13 +0200

    Remove broken git version check
    This also fixes scheduled notifications when running in a container because it keeps the main method from crashing
    before the scheduler is started.

--- mensabot/format.py
+++ mensabot/format.py
@@ -134,12 +134,5 @@ def check_legal_template(dir):
 
 def get_version():
     pkg_data = pkg_resources.require("mensabot")[0]
-    try:
-        git_rev = subprocess.check_output(
-            ["git", "describe", "--always"],
-            cwd=os.path.dirname(inspect.getfile(inspect.currentframe()))
-        ).decode('ascii').strip()
-    except NotADirectoryError:
-        git_rev = "release"
     # FIXME pkg_data.version is invalid when installed via "develop" and setup.py changes
-    return "{} {} {}".format(pkg_data.project_name.title(), pkg_data.version, git_rev)
+    return f"{pkg_data.project_name.title()} {pkg_data.version}"
